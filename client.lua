450d8ced9a77916cc782a36f677cb823685a1bb2
Hentari <49874824+Hentari@users.noreply.github.com>
2019-04-25 02:11:58 +0200

    Update client.lua
    Fixed visual effect

--- client.lua
+++ client.lua
@@ -1,36 +1,41 @@
 function setBleedingOn(ped)
-
-    SetEntityHealth(ped,GetEntityHealth(ped)-2)
-    StartScreenEffect('Rampage', 0, true)
-    ShakeGameplayCam("SMALL_EXPLOSION_SHAKE", 1.0)
-    InfoRanny("~r~Źle się czujesz jak najszyciej udaj się do medyka!")
-    SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
-    Wait(7000)
- 
- end
- 
- function setBleedingOff(ped)
-    StopScreenEffect('Rampage')
-    SetPlayerHealthRechargeMultiplier(PlayerId(), 1.0)
- end
- 
- Citizen.CreateThread(function()
- while true do
- Wait(0)
- local player = GetPlayerPed(-1)
- local Health = GetEntityHealth(player)
- 
-  if Health <= 139  then
-     setBleedingOn(player)
-      
-  elseif Health > 140 then
-    setBleedingOff(player)
+   SetEntityHealth(ped,GetEntityHealth(ped)-2)
+   if not effect then
+  StartScreenEffect('Rampage', 0, true)
+  effect = true
   end
+   ShakeGameplayCam("SMALL_EXPLOSION_SHAKE", 1.0)
+   InfoRanny("~r~Źle się czujesz jak najszybciej udaj się do medyka!")
+   SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+   Wait(7000)
+
+end
+
+function setBleedingOff(ped)
+   effect = false
+   StopScreenEffect('Rampage')
+   SetPlayerHealthRechargeMultiplier(PlayerId(), 1.0)
+end
+
+local effect = false
+
+Citizen.CreateThread(function()
+while true do
+Wait(0)
+local player = GetPlayerPed(-1)
+local Health = GetEntityHealth(player)
+
+ if Health <= 139  then
+    setBleedingOn(player)
+
+ elseif Health > 140 then
+   setBleedingOff(player)
  end
- end)
-  
- function InfoRanny(text)
- SetNotificationTextEntry("STRING")
- AddTextComponentString(text)
- DrawNotification(false, false)
- end
+end
+end)
+ 
+function InfoRanny(text)
+SetNotificationTextEntry("STRING")
+AddTextComponentString(text)
+DrawNotification(false, false)
+end
